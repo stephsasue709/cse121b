c15df61d5e9597af99beace760d814c7bb399543
stephanies709 <noe21002@byui.edu>
2022-05-27 13:44:37 -0400

    week4 completed

---
+++ week1/home.js
@@ -0,0 +1,15 @@
+// My name (h1)
+const myName = "Stephanie Sarah Noel";
+document.querySelector("#name").textContent = myName;
+
+// Title (h2)
+const myTitle = "CSE 121b: Assignment Portal";
+document.querySelector("#title").textContent = myTitle;
+
+// Current year (footer)
+const currentYear = 2022;
+document.querySelector("#year").textContent = currentYear;
+
+// Image
+const myPicture = "week1/image1/my_image.jpg"
+document.querySelector("img").setAttribute("src",myPicture); 
